5ef84b08f5cfa7284865302e3d56414560a0d1e4
EmekaEmmanuel <emekaekeohaa@gmail.com>
2023-03-28 10:17:35 -0700

    update readme.md

--- schema.sql
+++ schema.sql
@@ -12,3 +12,15 @@ CREATE TABLE animals (
     neutered BOOLEAN,
     weight_kg DECIMAL NOT NULL 
 );
+
+-- DAY TWO
+DROP TABLE animals;
+
+CREATE TABLE animals (
+id BIGSERIAL PRIMARY KEY NOT NULL,
+name VARCHAR(100) NOT NULL,
+species VARCHAR(50),
+date_of_birth DATE NOT NULL,
+weight_kg DECIMAL NOT NULL,
+neutered BOOLEAN NOT NULL,
+escape_attempts INT NOT NULL);
