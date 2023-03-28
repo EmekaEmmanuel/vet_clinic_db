84b6769ab3efff74a8a0d785b8cef99eff392f12
EmekaEmmanuel <emekaekeohaa@gmail.com>
2023-03-28 15:27:37 -0700

    update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,7 +1,7 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 /*  DAY 1 */
-SELECT * FROM animals WHERE name iLIKE '%mon';
+SELECT * FROM animals WHERE name LIKE '%mon';
 SELECT name FROM animals WHERE EXTRACT (year from date_of_birth) BETWEEN 2016 AND 2019;
 SELECT name FROM animals WHERE neutered = 'true' AND escape_attempts < '3';
 SELECT date_of_birth FROM animals WHERE (name = 'Agumon' OR name= 'Pikachu');
